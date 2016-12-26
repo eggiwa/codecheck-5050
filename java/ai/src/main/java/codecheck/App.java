7f434334b36b6a4f99d60954381a859f011b3803
code-project <dev@code-check.io>
2016-12-26 04:00:00 +0000

    Overwrite your files again

--- java/ai/src/main/java/codecheck/App.java
+++ java/ai/src/main/java/codecheck/App.java
@@ -1,9 +1,41 @@
 package codecheck;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			System.out.println(args[i]);
+
+		// try {
+
+		if(args.length < 1){
+			System.out.println("");
+			return;
 		}
+
+		ArrayList<String> list = new ArrayList<String>();
+		list.addAll(Arrays.asList(args));
+
+		if(list.size() <= 1){
+			System.out.println("");
+			return;
+		}
+		String target = list.remove(0);
+
+		for(String s : list) {
+			if(s.charAt(0) == target.charAt(target.length()-1)){
+				System.out.println(s);
+				return;
+			}
+		}
+
+		System.out.println("");
+		return;
+
+		// } catch (Exception e){
+		// 	return;
+		// }
+
+
 	}
 }
